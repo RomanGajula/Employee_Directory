31b703c8c4bd150cf075ac1facabc56b944dbb7c
roman_gajula <rgajula@bk.ru>
2021-05-04 12:41:33 +0300

    commit

--- app/src/main/java/com/example/employee_directory/model/GetRequest.kt
+++
@@ -1,6 +0,0 @@
-package com.example.employee_directory.model
-
-class GetRequest(
-        val data: List<Employee>,
-        val status: String
-)
--- app/src/main/java/com/example/employee_directory/model/PostRequest.kt
+++
@@ -1,6 +0,0 @@
-package com.example.employee_directory.model
-
-class PostRequest(
-        val data: Employee,
-        val status: String
-)
